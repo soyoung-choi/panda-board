61a46b9d2860e51a371bbd7312139ffa7914d54d
soyoung-choi <sydesign24@naver.com>
2022-06-15 03:35:18 +0900

    로그인폼 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,58 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e
+
+    if (name === 'email') {
+      setEmail(value)
+    } else if (name === 'password') {
+      setPassword(value)
+    }
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <form onSubmit={onSubmit} className="login-form">
+          <label for="email">이메일</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            placeholder="이메일을 입력해주세요."
+            autoFocus
+          />
+          <label for="password">비밀번호</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={email}
+            onChange={onChange}
+            placeholder="비밀번호를 입력해주세요."
+            autoFocus
+          />
+          <button>로그인</button>
+          <a href="http://localhost:3000/auth/kakao">
+            <img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg" />{' '}
+          </a>
+        </form>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
