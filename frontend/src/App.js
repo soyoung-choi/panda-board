fc37add038c2b35e07ac6000424844089536a48a
soyoung-choi <sydesign24@naver.com>
2022-06-16 00:24:02 +0900

    로그인페이지 디자인

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import { useState } from 'react'
 import 'styles/main.scss'
+import panda from 'images/panda.svg'
+import kakaoIcon from 'images/icon-kakao.svg'
 
 function App() {
   const [email, setEmail] = useState('')
@@ -25,6 +27,15 @@ function App() {
   return (
     <>
       <div>
+        <div className="panda" style={{ width: '230px', margin: '50px auto' }}>
+          <img
+            src={panda}
+            alt="판다"
+            style={{ width: '100%', marginBottom: '10px' }}
+          />
+          <h1 style={{ fontSize: '36px', textAlign: 'center' }}>Panda Board</h1>
+        </div>
+
         <form onSubmit={onSubmit} className="login-form">
           <label for="email">이메일</label>
           <input
@@ -47,8 +58,10 @@ function App() {
           />
           <button>로그인</button>
         </form>
-        <a href="http://localhost:3000/auth/kakao">
-          <img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg" />{' '}
+
+        <a className="btn btn-kakao" href="http://localhost:3000/auth/kakao">
+          <img src={kakaoIcon} alt="카카오 아이콘" />
+          <span>카카오 로그인</span>
         </a>
       </div>
     </>
